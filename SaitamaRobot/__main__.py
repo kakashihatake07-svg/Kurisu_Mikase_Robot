9dbf0ae3212889ff41ee5315fdb81274548ad5b7
Sidd <77623761+Sidd-Pixil@users.noreply.github.com>
2021-03-05 08:48:38 +0530

    Updated AF
    i am online gif no. 537

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -534,7 +534,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[I am now online!](https://telegra.ph/file/26aeb38f38eb8c819e423.mp4)", parse_mode=ParseMode.MARKDOWN)
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[I am now online!](https://telegra.ph/file/c2dcfa1a945502566db0f.gif)", parse_mode=ParseMode.MARKDOWN)
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!")
